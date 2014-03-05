9ae75e8d9c56710d2f2cb3d2f2ea33496d8f9c08
Jonny Lamb <jonny.lamb@collabora.co.uk>
2014-03-05 12:44:38 +0100

    shell: add missing copyright headers to files

--- shell/gtk-shell.c
+++ shell/gtk-shell.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2013 Tiago Vignatti
+ * Copyright (c) 2013-2014 Collabora Ltd.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
